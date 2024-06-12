1e104e32c09441e6d5fe9acfd1f95cda6ce39a40
ShirleyRM <srm.shirley@gmail.com>
2024-06-11 19:45:55 -0500

    feat: add burger menu

--- src/main.js
+++ src/main.js
@@ -16,3 +16,14 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+// Close and open burger menu
+const burgerMenu = document.querySelector("#burgerMenu");
+const header = document.querySelector("#header");
+const showList = document.querySelector("#showList");
+
+burgerMenu.addEventListener("click", () => {
+  header.classList.toggle("showMenu");
+  showList.classList.toggle("openList");
+  burgerMenu.classList.toggle("change");
+});
