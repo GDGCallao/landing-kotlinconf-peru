31836fb536a8807e6bc5b5a065663b6cccbd344b
ShirleyRM <srm.shirley@gmail.com>
2024-06-11 20:00:13 -0500

    feat: add active class and change it when scroll into sections

--- src/main.js
+++ src/main.js
@@ -1,20 +1,51 @@
+// Add active class
 document.addEventListener("DOMContentLoaded", function () {
-  const navLinks = document.querySelectorAll("#nav a");
+  const navLinks = document.querySelectorAll(".nav_list a");
+  const sections = document.querySelectorAll("section");
+
+  function removeActiveClasses() {
+    navLinks.forEach((link) => link.classList.remove("active"));
+  }
+
+  function addActiveClass(link) {
+    removeActiveClasses();
+    link.classList.add("active");
+  }
 
   navLinks.forEach((link) => {
     link.addEventListener("click", function (event) {
 
       event.preventDefault();
 
-      navLinks.forEach((link) => link.classList.remove("active"));
-
-      this.classList.add("active");
+      addActiveClass(this);
 
       document.querySelector(this.getAttribute("href")).scrollIntoView({
         behavior: "smooth",
       });
     });
   });
+
+// Change active class when scrolling into sections
+  function highlightNavOnScroll() {
+    let scrollPosition = window.scrollY + 70;
+
+    sections.forEach((section) => {
+      if (
+        scrollPosition >= section.offsetTop &&
+        scrollPosition < section.offsetTop + section.offsetHeight
+      ) {
+        let currentId = section.getAttribute("id");
+        let currentLink = document.querySelector(
+          `.nav_list a[href="#${currentId}"]`
+        );
+        addActiveClass(currentLink);
+      }
+    });
+  }
+
+  window.addEventListener("scroll", highlightNavOnScroll);
+
+  highlightNavOnScroll();
 });
 
 // Close and open burger menu
